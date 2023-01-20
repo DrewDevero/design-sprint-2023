a95ef2f251370d90e0f375b9ecbb0f01cad08a7a
Alston Devero-Belfon <andwele@alstons-mbp.mynetworksettings.com>
2023-01-20 09:50:16 -0500

    altered app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Harris Tweed mock site', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/HARRIS TWEED/i);
   expect(linkElement).toBeInTheDocument();
 });
